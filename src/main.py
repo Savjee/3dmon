f7a55d310814d8349e7fd37a16c41e25c71fbd77
Xavier Decuyper <xavier.decuyper@gmail.com>
2020-04-14 10:20:53 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,54 @@
+import serial
+import requests
+import json
+
+###
+## CONFIGURATION
+###
+INTERFACE = "/dev/ttyUSB0"
+
+ENTITY_ID = "sensor.prusa_print_progress"
+FRIENDLY_NAME = "Prusa i3 MK3"
+
+API_URL = "http://192.168.2.95:8123/api"
+API_TOKEN = "xxxxx"
+
+headers = {"Authorization": "Bearer " + API_TOKEN, "Content-Type": "application/json"}
+
+###
+# This functions updates the entity in Home Assistant
+##
+def sendToHA(percent, timeLeft):
+	data = {
+		"state": percent,
+		"attributes": {
+			"unit_of_measurement": "%",
+			"friendly_name": FRIENDLY_NAME,
+			"timeLeft": timeLeft
+		}
+	}
+
+	print("Posting this to HA: " + json.dumps(data));
+	r = requests.post(url = API_URL + "/states/" + ENTITY_ID, headers=headers, data=json.dumps(data))
+	print(r.text)
+
+
+###
+# Start listening to the serial interface and read lines!
+###
+regex = r"NORMAL MODE: Percent done: (.*); print time remaining in mins: (.*)"
+
+ser = serial.Serial(INTERFACE)
+print(ser.name);
+
+with serial.Serial(INTERFACE, 115200, timeout=1) as ser:
+	while True:
+		line = ser.readline()
+		print(line)
+
+		match = re.match(regex, line)
+
+		if match:
+			percent = match.group(1)
+			timeLeft = match.group(2)
+			sendToHA(percent, timeLeft)
